c8fc006cd990c1b8178d177dc8d0e35ea1ba5c2f
Lovinder Pnag <lspnag@gmail.com>
2014-10-15 19:27:50 -0500

    added models and migrations

---
+++ db/migrate/20141016001821_create_sections.rb
@@ -0,0 +1,11 @@
+class CreateSections < ActiveRecord::Migration
+  def change
+    create_table :sections do |t|
+      t.integer :teacher_id
+      t.integer :class_id
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
