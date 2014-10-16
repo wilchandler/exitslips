88c892cd0c49b557b1f3969e7cb2934654293ffe
Colin Shevlin <cwshevlin@gmail.com>
2014-10-15 20:06:39 -0500

    Added initial routes and views.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,16 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  root => 'user#login'
+
+  resources :teachers
+
+  resources :sections do
+    resources :quizzes
+    resources :students
+  end
+
+
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
