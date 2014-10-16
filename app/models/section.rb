37a0d8c6c5b07dbf32408969aea171fb5b27bee1
Lovinder Pnag <lspnag@gmail.com>
2014-10-15 20:06:22 -0500

    added rough draft of associations

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,2 +1,6 @@
 class Section < ActiveRecord::Base
+	has_many :section_teachers
+	has_many :teachers, through: :section_teachers, class_name: "User"
+	has_many :standards
+	has_many :quizzes
 end
--- app/models/standard.rb
+++ app/models/standard.rb
@@ -1,2 +1,4 @@
 class Standard < ActiveRecord::Base
+	belongs_to :section
+	has_many :questions
 end
