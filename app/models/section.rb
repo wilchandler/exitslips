e866a03dcc794b76e612f262163e261e4b0bafa2
Lovinder Pnag <lspnag@gmail.com>
2014-10-15 20:36:49 -0500

    udpate

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,6 +1,6 @@
 class Section < ActiveRecord::Base
 	has_many :section_teachers
-	has_many :teachers, through: :section_teachers, class_name: "User"
+	has_many :teachers, through: :section_teachers
 	has_many :standards
 	has_many :quizzes
 end
