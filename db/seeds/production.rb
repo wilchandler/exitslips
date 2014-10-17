646163f890ecfe8e7b04c9307cced5d5f08b02dc
Colin Shevlin <cwshevlin@gmail.com>
2014-10-17 14:16:37 -0500

    IT IS SO LOUD IN HERE. Also I added better regex for the standards seeds.

--- db/seeds/production.rb
+++ db/seeds/production.rb
@@ -5,10 +5,10 @@ require 'csv'
 
 CSV.foreach("db/ela_standards.csv") do |row|
 	unless row.last == nil || row.first[0] == '('
-	# standard = Standard.new(type: "ELA", grade: (/^\d+[-\d]*/).match(row.first).to_s, code: row.first, description: row.last)
+	# standard = Standard.new(subject: "ELA", grade: (/^\d+[-\d]*/).match(row.first).to_s, code: row.first, description: row.last)
 	# standard.save unless standard.grade.nil?
-		puts "type: ELA"
-		puts "grade: #{(/^\d+[-\d]*/).match(row.first).to_s}"
+		puts "subject: ELA"
+		puts "grade: #{(/^(?!-)(\d|K)+/).match(row.first).to_s}"
 		puts "code: #{row.first}"
 		puts "description: #{row.last}"
 	end
@@ -18,10 +18,10 @@ CSV.foreach("db/math_standards.csv") do |row|
 
 
 	unless row.last == nil || row.first[0] == '('
-	# standard = Standard.new(type: "ELA", grade: (/^\d+[-\d]*/).match(row.first).to_s, code: row.first, description: row.last)
+	# standard = Standard.new(subject: "Math", grade: (/^\d+[-\d]*/).match(row.first).to_s, code: row.first, description: row.last)
 	# standard.save unless standard.grade.nil?
-		puts "type: ELA"
-		puts "grade: #{(/^\d+[-\d]*/).match(row.first).to_s}"
+		puts "subject: Math"
+		puts "grade: #{(/^(?!-)(\d|K)+/).match(row.first).to_s}"
 		puts "code: #{row.first}"
 		puts "description: #{row.last}"
 	end
