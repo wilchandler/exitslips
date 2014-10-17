2ef21cddc211e96d4865fadf2d75bae3c81c3de2
Colin Shevlin <cwshevlin@gmail.com>
2014-10-17 18:26:48 -0500

    Adding some filters to the sessions controller.

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -2,61 +2,42 @@ class SectionsController < ApplicationController
 	include SectionsHelper
 
 	def index
-
 		student_leave
 		@sections = Section.all
 		@teacher = current_user
-	   @sections = current_user.sections
-	   @quizzes = current_user.quizzes
+	  @sections = current_user.sections
+	  @quizzes = current_user.quizzes
 	end
-
-
-
 
 	def show
 		logged_in?
 		student_leave
 		@section = Section.find(params[:id])
-		
-
-
 	end
 
+  def confirm
+  	@section = Section.find(params[:section_id])
+  end
 
-
-
-
-
-	
-
-
-    def confirm
-    	@section = Section.find(params[:section_id])
-    end
-
-    def confirmed
-    	@section = Section.find(params[:section_id])
-    	if params[:passcode] = @section.passcode ||
-    		Enrollment.create(section_id:@section.id,student_id:current_user.id)
-    		redirect_to section_path(@section)
-    	else
-    		redirect_to sections_path
-
+  def confirmed
+  	@section = Section.find(params[:section_id])
+  	if params[:passcode] = @section.passcode ||
+  		Enrollment.create(section_id:@section.id,student_id:current_user.id)
+  		redirect_to section_path(@section)
+  	else
+  		redirect_to sections_path
 		end
+  end
 
-    end
-
-    def new
-    	logged_in?
-     	student_leave
-     	@section = Section.new
+  def new
+		logged_in?
+	 	student_leave
+	 	@section = Section.new
 		@teacher = current_user
-	    @sections = current_user.sections
-	    @quizzes = current_user.quizzes
+	  @sections = current_user.sections
+	  @quizzes = current_user.quizzes
 	 
 	end
-
-
 
 	def create
 		@section = Section.new(section_params)
@@ -67,12 +48,10 @@ class SectionsController < ApplicationController
 	end
 
 
-
-
 	def edit
-	    logged_in?
-	    student_leave
-	    @section = Section.find(params[:id])
+	  logged_in?
+	  student_leave
+	  @section = Section.find(params[:id])
 	end
 
 	def update
@@ -80,9 +59,9 @@ class SectionsController < ApplicationController
 		@section.update(section_params)
 		if @section.save
 			redirect_to sections_path
-	    else
-	    	render 'edit'
-	    end
+    else
+    	render 'edit'
+    end
 	end
 
 	def destroy
@@ -97,7 +76,6 @@ class SectionsController < ApplicationController
 	private
     def section_params
     	params.require(:section).permit(:name,:subject,:grade)
-
     end
 end
 
