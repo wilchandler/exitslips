8133e3e33e9366755e7f3a41e3c4c1ba1ee5e381
Colin Shevlin <cwshevlin@gmail.com>
2014-10-17 15:43:46 -0500

    Fixed a bug in logging in, fixed up the tacher view.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,13 @@
 class HomeController < ApplicationController
   def index
   	# redirect_to teacher_path(current_user) if user_signed_in?
-
     if user_signed_in? && current_user.class == Teacher
+    	debugger
     	redirect_to sections_path
     elsif user_signed_in? && current_user.class == Student
+    	debugger
     	redirect_to quizzes_path
     end
+    debugger
   end
 end
