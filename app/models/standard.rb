8c464dc035da38b4310c2f596f9dd4ee861a83b4
Wil Chandler <wilchandler2@gmail.com>
2014-10-17 14:28:06 -0500

    Added seeds, removed standards validation error

--- app/models/standard.rb
+++ app/models/standard.rb
@@ -2,5 +2,5 @@ class Standard < ActiveRecord::Base
 	has_many :quizzes
   has_many :sections, through: :quizzes
 
-  validates :type, :grade, :code, :description, presence: true
+  validates :subject, :grade, :code, :description, presence: true
 end
