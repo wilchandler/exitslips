372544413bc70e1865e669f237be9701c3003180
Colin Shevlin <cwshevlin@gmail.com>
2014-10-17 14:50:10 -0500

    Adding standards from the CSV is working.

--- db/seeds/production.rb
+++ db/seeds/production.rb
@@ -1,28 +1,15 @@
 require 'csv'
 
-
-# Standard: type:. grade: code:, decription:,
-
 CSV.foreach("db/ela_standards.csv") do |row|
 	unless row.last == nil || row.first[0] == '('
-		p standard = Standard.new(subject: "ELA", grade: (/^\d+[-\d]*/).match(row.first).to_s, code: row.first, description: row.last)
-		p standard.save unless standard.grade.nil?
-		# puts "subject: ELA"
-		# puts "grade: #{(/^(?!-)(\d|K)+/).match(row.first).to_s}"
-		# puts "code: #{row.first}"
-		# puts "description: #{row.last}"
+		standard = Standard.new(subject: "ELA", grade: (/^(?!-)(\d+|K)/).match(row.first).to_s, code: row.first, description: row.last)
+		standard.save unless standard.grade.nil?
 	end
 end
 
 CSV.foreach("db/math_standards.csv") do |row|
-
-
 	unless row.last == nil || row.first[0] == '('
-	  p standard = Standard.new(subject: "Math", grade: (/^\d+[-\d]*/).match(row.first).to_s, code: row.first, description: row.last)
-	  p standard.save unless standard.grade.nil?
-		# puts "subject: Math"
-		# puts "grade: #{(/^(?!-)(\d|K)+/).match(row.first).to_s}"
-		# puts "code: #{row.first}"
-		# puts "description: #{row.last}"
+	  standard = Standard.new(subject: "Math", grade: (/^(?!-)(\d+|K)/).match(row.first).to_s, code: row.first, description: row.last)
+	  standard.save unless standard.grade.nil?
 	end
 end
