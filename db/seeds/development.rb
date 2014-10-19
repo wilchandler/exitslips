3da8eedb4a6bce2468c3befe4033cd1eb273ef67
Colin Shevlin <cwshevlin@gmail.com>
2014-10-18 19:51:49 -0500

    Addded lots of seeds so we can see kool data.

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -146,14 +146,57 @@ Option.create!(question: q14, content: "105%", correct?: false)
 
 sixth_quiz12 = Quiz.create!(name: "Ratios and Percents II", instructions: "Do these, children!", standard: sixth_standard2, section_id: 4)
 
-sixth_quiz3 = Quiz.create!(name: Faker::Lorem.word.capitalize, instructions: "Do these, children!", standard: sixth_standard3, section_id: 4)
-sixth_quiz4 = Quiz.create!(name: Faker::Lorem.word.capitalize, instructions: "Do these, children!", standard: sixth_standard4, section_id: 4)
-sixth_quiz5 = Quiz.create!(name: Faker::Lorem.word.capitalize, instructions: "Do these, children!", standard: sixth_standard5, section_id: 4)
-sixth_quiz6 = Quiz.create!(name: Faker::Lorem.word.capitalize, instructions: "Do these, children!", standard: sixth_standard6, section_id: 4)
-sixth_quiz7 = Quiz.create!(name: Faker::Lorem.word.capitalize, instructions: "Do these, children!", standard: sixth_standard7, section_id: 4)
-sixth_quiz8 = Quiz.create!(name: Faker::Lorem.word.capitalize, instructions: "Do these, children!", standard: sixth_standard8, section_id: 4)
-sixth_quiz9 = Quiz.create!(name: Faker::Lorem.word.capitalize, instructions: "Do these, children!", standard: sixth_standard9, section_id: 4)
-sixth_quiz10 = Quiz.create!(name: Faker::Lorem.word.capitalize, instructions: "Do these, children!", standard: sixth_standard10, section_id: 4)
+sixth_quiz13 = Quiz.create!(name: Faker::Lorem.word.capitalize, instructions: "Do these, children!", standard: sixth_standard3, section_id: 4)
+sixth_quiz14 = Quiz.create!(name: Faker::Lorem.word.capitalize, instructions: "Do these, children!", standard: sixth_standard4, section_id: 4)
+sixth_quiz15 = Quiz.create!(name: Faker::Lorem.word.capitalize, instructions: "Do these, children!", standard: sixth_standard5, section_id: 4)
+sixth_quiz16 = Quiz.create!(name: Faker::Lorem.word.capitalize, instructions: "Do these, children!", standard: sixth_standard6, section_id: 4)
+sixth_quiz17 = Quiz.create!(name: Faker::Lorem.word.capitalize, instructions: "Do these, children!", standard: sixth_standard7, section_id: 4)
+sixth_quiz18 = Quiz.create!(name: Faker::Lorem.word.capitalize, instructions: "Do these, children!", standard: sixth_standard8, section_id: 4)
+sixth_quiz19 = Quiz.create!(name: Faker::Lorem.word.capitalize, instructions: "Do these, children!", standard: sixth_standard9, section_id: 4)
+sixth_quiz20 = Quiz.create!(name: Faker::Lorem.word.capitalize, instructions: "Do these, children!", standard: sixth_standard10, section_id: 4)
+
+sixth_quiz21 = Quiz.create!(name: "Ratios and Percents I", instructions: "Do these, children!", standard: sixth_standard1, section_id: 5)
+q15 = Question.create!(quiz: sixth_quiz1, query: "60% of what number is 45", question_type: "multiple_choice")
+Option.create!(question: q15, content: "27", correct?: false)
+Option.create!(question: q15, content: "30", correct?: false)
+Option.create!(question: q15, content: "60", correct?: false)
+Option.create!(question: q15, content: "75", correct?: true)
+Option.create!(question: q15, content: "90", correct?: false)
+q16 = Question.create!(quiz: sixth_quiz1, query: "What percent of 48 is 60?", question_type: "multiple_choice")
+Option.create!(question: q16, content: "65%", correct?: false)
+Option.create!(question: q16, content: "80%", correct?: false)
+Option.create!(question: q16, content: "85%", correct?: false)
+Option.create!(question: q16, content: "90%", correct?: false)
+Option.create!(question: q16, content: "125%", correct?: true)
+q17 = Question.create!(quiz: sixth_quiz1, query: "What number is 5% of 2000?", question_type: "multiple_choice")
+Option.create!(question: q17, content: "50", correct?: false)
+Option.create!(question: q17, content: "100", correct?: true)
+Option.create!(question: q17, content: "150", correct?: false)
+Option.create!(question: q17, content: "200", correct?: false)
+Option.create!(question: q17, content: "250", correct?: false)
+q18 = Question.create!(quiz: sixth_quiz1, query: "35% of what number is 70?", question_type: "multiple_choice")
+Option.create!(question: q18, content: "100", correct?: false)
+Option.create!(question: q18, content: "110", correct?: false)
+Option.create!(question: q18, content: "150", correct?: false)
+Option.create!(question: q18, content: "200", correct?: true)
+Option.create!(question: q18, content: "250", correct?: false)
+q19 = Question.create!(quiz: sixth_quiz1, query: "Jim works for $15.50 per hour for a health care facility. He is supposed to get a 75 cent per hour raise at one year of service. What will his percent increase in hourly pay be?", question_type: "multiple_choice")
+Option.create!(question: q19, content: "2.7%", correct?: false)
+Option.create!(question: q19, content: "3.3%", correct?: false)
+Option.create!(question: q19, content: "133%", correct?: false)
+Option.create!(question: q19, content: "4.8%", correct?: true)
+Option.create!(question: q19, content: "105%", correct?: false)
+
+sixth_quiz22 = Quiz.create!(name: "Ratios and Percents II", instructions: "Do these, children!", standard: sixth_standard2, section_id: 5)
+
+sixth_quiz23 = Quiz.create!(name: Faker::Lorem.word.capitalize, instructions: "Do these, children!", standard: sixth_standard3, section_id: 5)
+sixth_quiz24 = Quiz.create!(name: Faker::Lorem.word.capitalize, instructions: "Do these, children!", standard: sixth_standard4, section_id: 5)
+sixth_quiz25 = Quiz.create!(name: Faker::Lorem.word.capitalize, instructions: "Do these, children!", standard: sixth_standard5, section_id: 5)
+sixth_quiz26 = Quiz.create!(name: Faker::Lorem.word.capitalize, instructions: "Do these, children!", standard: sixth_standard6, section_id: 5)
+sixth_quiz27 = Quiz.create!(name: Faker::Lorem.word.capitalize, instructions: "Do these, children!", standard: sixth_standard7, section_id: 5)
+sixth_quiz28 = Quiz.create!(name: Faker::Lorem.word.capitalize, instructions: "Do these, children!", standard: sixth_standard8, section_id: 5)
+sixth_quiz29 = Quiz.create!(name: Faker::Lorem.word.capitalize, instructions: "Do these, children!", standard: sixth_standard9, section_id: 5)
+sixth_quiz30 = Quiz.create!(name: Faker::Lorem.word.capitalize, instructions: "Do these, children!", standard: sixth_standard10, section_id: 5)
 
 
 
@@ -162,14 +205,35 @@ sixth_quiz10 = Quiz.create!(name: Faker::Lorem.word.capitalize, instructions: "D
   s.sections << section3
 
 
-  sitting1 = Sitting.create!(student: s, quiz: sixth_quiz1, possible: 5, correct: rand(5))
-  sitting2 = Sitting.create!(student: s, quiz: sixth_quiz2, possible: 2, correct: rand(2))
-  sitting3 = Sitting.create!(student: s, quiz: , possible: 1, correct: 1)
+  Sitting.create!(student: s, quiz: sixth_quiz1, possible: 5, correct: rand(5))
+  Sitting.create!(student: s, quiz: sixth_quiz2, possible: 2, correct: rand(2))
+  Sitting.create!(student: s, quiz: sixth_quiz3, possible: 10, correct: rand(10))
+  Sitting.create!(student: s, quiz: sixth_quiz4, possible: 10, correct: rand(10))
+  Sitting.create!(student: s, quiz: sixth_quiz5, possible: 10, correct: rand(10))
+  Sitting.create!(student: s, quiz: sixth_quiz6, possible: 10, correct: rand(10))
+  Sitting.create!(student: s, quiz: sixth_quiz7, possible: 10, correct: rand(10))
+  Sitting.create!(student: s, quiz: sixth_quiz8, possible: 10, correct: rand(10))
+  Sitting.create!(student: s, quiz: sixth_quiz9, possible: 10, correct: rand(10))
+  Sitting.create!(student: s, quiz: sixth_quiz10, possible: 10, correct: rand(10))
 
-  Answer.create!(question_id: q1, content: "4", correct?: true, student: s, sitting: sitting1)
-  Answer.create!(question_id: q2, content: "14", correct?: false, student: s, sitting: sitting1)
-  Answer.create!(question_id: q3, content: "1054.35", correct?: true, student: s, sitting: sitting2)
-  Answer.create!(question_id: q4, content: "Your", correct?: true, student: s, sitting: sitting3)
+  s.save
+end
+
+30.times do
+  s = Student.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, password: "12345678", email: Faker::Internet.email)
+  s.sections << section4
+
+
+  Sitting.create!(student: s, quiz: sixth_quiz11, possible: 5, correct: rand(5))
+  Sitting.create!(student: s, quiz: sixth_quiz12, possible: 2, correct: rand(2))
+  Sitting.create!(student: s, quiz: sixth_quiz13, possible: 10, correct: rand(10))
+  Sitting.create!(student: s, quiz: sixth_quiz14, possible: 10, correct: rand(10))
+  Sitting.create!(student: s, quiz: sixth_quiz15, possible: 10, correct: rand(10))
+  Sitting.create!(student: s, quiz: sixth_quiz16, possible: 10, correct: rand(10))
+  Sitting.create!(student: s, quiz: sixth_quiz17, possible: 10, correct: rand(10))
+  Sitting.create!(student: s, quiz: sixth_quiz18, possible: 10, correct: rand(10))
+  Sitting.create!(student: s, quiz: sixth_quiz19, possible: 10, correct: rand(10))
+  Sitting.create!(student: s, quiz: sixth_quiz20, possible: 10, correct: rand(10))
 
   s.save
 end
