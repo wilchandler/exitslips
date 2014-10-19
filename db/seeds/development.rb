61d6279c1d2752e835b76fdb47f60bc14b87945d
Colin Shevlin <cwshevlin@gmail.com>
2014-10-18 19:32:06 -0500

    Added lots of quiz seeds so we can have lots of data.

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -68,7 +68,6 @@ sixth_standard9 = Standard.find_by_code("6.EE.6")
 sixth_standard10 = Standard.find_by_code("6.EE.7")
 sixth_standard10 = Standard.find_by_code("6.G.1")
 
-
 sixth_quiz1 = Quiz.create!(name: "Ratios and Percents I", instructions: "Do these, children!", standard: sixth_standard1, section_id: 3)
 q5 = Question.create!(quiz: sixth_quiz1, query: "60% of what number is 45", question_type: "multiple_choice")
 Option.create!(question: q5, content: "27", correct?: false)
@@ -94,9 +93,85 @@ Option.create!(question: q8, content: "110", correct?: false)
 Option.create!(question: q8, content: "150", correct?: false)
 Option.create!(question: q8, content: "200", correct?: true)
 Option.create!(question: q8, content: "250", correct?: false)
-q8 = Question.create!(quiz: sixth_quiz1, query: "35% of what number is 70?", question_type: "multiple_choice")
-Option.create!(question: q8, content: "100", correct?: false)
-Option.create!(question: q8, content: "110", correct?: false)
-Option.create!(question: q8, content: "150", correct?: false)
-Option.create!(question: q8, content: "200", correct?: true)
-Option.create!(question: q8, content: "250", correct?: false)
+q9 = Question.create!(quiz: sixth_quiz1, query: "Jim works for $15.50 per hour for a health care facility. He is supposed to get a 75 cent per hour raise at one year of service. What will his percent increase in hourly pay be?", question_type: "multiple_choice")
+Option.create!(question: q9, content: "2.7%", correct?: false)
+Option.create!(question: q9, content: "3.3%", correct?: false)
+Option.create!(question: q9, content: "133%", correct?: false)
+Option.create!(question: q9, content: "4.8%", correct?: true)
+Option.create!(question: q9, content: "105%", correct?: false)
+
+sixth_quiz2 = Quiz.create!(name: "Ratios and Percents II", instructions: "Do these, children!", standard: sixth_standard2, section_id: 3)
+
+sixth_quiz3 = Quiz.create!(name: Faker::Lorem.word.capitalize, instructions: "Do these, children!", standard: sixth_standard3, section_id: 3)
+sixth_quiz4 = Quiz.create!(name: Faker::Lorem.word.capitalize, instructions: "Do these, children!", standard: sixth_standard4, section_id: 3)
+sixth_quiz5 = Quiz.create!(name: Faker::Lorem.word.capitalize, instructions: "Do these, children!", standard: sixth_standard5, section_id: 3)
+sixth_quiz6 = Quiz.create!(name: Faker::Lorem.word.capitalize, instructions: "Do these, children!", standard: sixth_standard6, section_id: 3)
+sixth_quiz7 = Quiz.create!(name: Faker::Lorem.word.capitalize, instructions: "Do these, children!", standard: sixth_standard7, section_id: 3)
+sixth_quiz8 = Quiz.create!(name: Faker::Lorem.word.capitalize, instructions: "Do these, children!", standard: sixth_standard8, section_id: 3)
+sixth_quiz9 = Quiz.create!(name: Faker::Lorem.word.capitalize, instructions: "Do these, children!", standard: sixth_standard9, section_id: 3)
+sixth_quiz10 = Quiz.create!(name: Faker::Lorem.word.capitalize, instructions: "Do these, children!", standard: sixth_standard10, section_id: 3)
+
+
+sixth_quiz11 = Quiz.create!(name: "Ratios and Percents I", instructions: "Do these, children!", standard: sixth_standard1, section_id: 4)
+q10 = Question.create!(quiz: sixth_quiz1, query: "60% of what number is 45", question_type: "multiple_choice")
+Option.create!(question: q10, content: "27", correct?: false)
+Option.create!(question: q10, content: "30", correct?: false)
+Option.create!(question: q10, content: "60", correct?: false)
+Option.create!(question: q10, content: "75", correct?: true)
+Option.create!(question: q10, content: "90", correct?: false)
+q11 = Question.create!(quiz: sixth_quiz1, query: "What percent of 48 is 60?", question_type: "multiple_choice")
+Option.create!(question: q11, content: "65%", correct?: false)
+Option.create!(question: q11, content: "80%", correct?: false)
+Option.create!(question: q11, content: "85%", correct?: false)
+Option.create!(question: q11, content: "90%", correct?: false)
+Option.create!(question: q11, content: "125%", correct?: true)
+q12 = Question.create!(quiz: sixth_quiz1, query: "What number is 5% of 2000?", question_type: "multiple_choice")
+Option.create!(question: q12, content: "50", correct?: false)
+Option.create!(question: q12, content: "100", correct?: true)
+Option.create!(question: q12, content: "150", correct?: false)
+Option.create!(question: q12, content: "200", correct?: false)
+Option.create!(question: q12, content: "250", correct?: false)
+q13 = Question.create!(quiz: sixth_quiz1, query: "35% of what number is 70?", question_type: "multiple_choice")
+Option.create!(question: q13, content: "100", correct?: false)
+Option.create!(question: q13, content: "110", correct?: false)
+Option.create!(question: q13, content: "150", correct?: false)
+Option.create!(question: q13, content: "200", correct?: true)
+Option.create!(question: q13, content: "250", correct?: false)
+q14 = Question.create!(quiz: sixth_quiz1, query: "Jim works for $15.50 per hour for a health care facility. He is supposed to get a 75 cent per hour raise at one year of service. What will his percent increase in hourly pay be?", question_type: "multiple_choice")
+Option.create!(question: q14, content: "2.7%", correct?: false)
+Option.create!(question: q14, content: "3.3%", correct?: false)
+Option.create!(question: q14, content: "133%", correct?: false)
+Option.create!(question: q14, content: "4.8%", correct?: true)
+Option.create!(question: q14, content: "105%", correct?: false)
+
+sixth_quiz12 = Quiz.create!(name: "Ratios and Percents II", instructions: "Do these, children!", standard: sixth_standard2, section_id: 4)
+
+sixth_quiz3 = Quiz.create!(name: Faker::Lorem.word.capitalize, instructions: "Do these, children!", standard: sixth_standard3, section_id: 4)
+sixth_quiz4 = Quiz.create!(name: Faker::Lorem.word.capitalize, instructions: "Do these, children!", standard: sixth_standard4, section_id: 4)
+sixth_quiz5 = Quiz.create!(name: Faker::Lorem.word.capitalize, instructions: "Do these, children!", standard: sixth_standard5, section_id: 4)
+sixth_quiz6 = Quiz.create!(name: Faker::Lorem.word.capitalize, instructions: "Do these, children!", standard: sixth_standard6, section_id: 4)
+sixth_quiz7 = Quiz.create!(name: Faker::Lorem.word.capitalize, instructions: "Do these, children!", standard: sixth_standard7, section_id: 4)
+sixth_quiz8 = Quiz.create!(name: Faker::Lorem.word.capitalize, instructions: "Do these, children!", standard: sixth_standard8, section_id: 4)
+sixth_quiz9 = Quiz.create!(name: Faker::Lorem.word.capitalize, instructions: "Do these, children!", standard: sixth_standard9, section_id: 4)
+sixth_quiz10 = Quiz.create!(name: Faker::Lorem.word.capitalize, instructions: "Do these, children!", standard: sixth_standard10, section_id: 4)
+
+
+
+30.times do
+  s = Student.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, password: "12345678", email: Faker::Internet.email)
+  s.sections << section3
+
+
+  sitting1 = Sitting.create!(student: s, quiz: sixth_quiz1, possible: 5, correct: rand(5))
+  sitting2 = Sitting.create!(student: s, quiz: sixth_quiz2, possible: 2, correct: rand(2))
+  sitting3 = Sitting.create!(student: s, quiz: , possible: 1, correct: 1)
+
+  Answer.create!(question_id: q1, content: "4", correct?: true, student: s, sitting: sitting1)
+  Answer.create!(question_id: q2, content: "14", correct?: false, student: s, sitting: sitting1)
+  Answer.create!(question_id: q3, content: "1054.35", correct?: true, student: s, sitting: sitting2)
+  Answer.create!(question_id: q4, content: "Your", correct?: true, student: s, sitting: sitting3)
+
+  s.save
+end
+
+
