2c92eb9c1905821395e781d39644e816fa3d304e
Wil Chandler <wilchandler2@gmail.com>
2014-10-20 15:08:54 -0500

    Added rails JSON response to grouping request (with passing tests)

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   end
 
   get "/sections/:section_id/quizzes/:id", to: "quizzes#results"
+  get "/sections/:section_id/quizzes/:id/groups", to: "quizzes#groups"
 
   root to: 'home#index'
 
