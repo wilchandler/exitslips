f591f07cde4f032584e02ccd5f7a5dc0c70b326c
Lovinder Pnag <lspnag@gmail.com>
2014-10-23 10:01:54 -0500

    working on tests

--- spec/models/teacher_spec.rb
+++ spec/models/teacher_spec.rb
@@ -14,11 +14,49 @@ RSpec.describe Teacher, :type => :model do
 		@teacher = Teacher.create!(first_name: "Liberty", last_name: "Valance", email: "yahoo@gmail.com", password: "12345678")
     @section = Section.create!(teacher_id: 1, name: "Yabadabadoo", grade: "K", subject: "Math")
     @standard = Standard.create!(subject: "Math", grade: "K", description: "really hard stuff", code: "K.RL.1")
-    @requirement = Requirement.create!(standard_id: @standard.id, section_id: @section.id)
+    @requirement = Requirement.create!(standard_id: 1, section_id: 1)
     @student1 = Student.create!(first_name: "student", last_name: "1", email: "tester1@yahoo.com", password: "12345678")
-    @quiz = Quiz.create!(name: "MyQuiz", instructions: "My Instructions", section: @section.id, requirement_id: 1)
+    @student2 = Student.create!(first_name: "student1", last_name: "2", email: "tester2@yahoo.com", password: "12345678")
+    @student3 = Student.create!(first_name: "student2", last_name: "3", email: "tester3@yahoo.com", password: "12345678")
+    @enrollment = Enrollment.create!(student_id: 1, section_id: 1)
+    @quiz = Quiz.create!(name: "MyQuiz", instructions: "My Instructions", section_id: 1, requirement_id: 1)
+    @quiz2 = Quiz.create!(name: "MyQuiz2", instructions: "My Instructions", section_id: 1, requirement_id: 1)
+    @answers = 
 
     @sitting = Sitting.create!(student_id: 1, quiz_id: 1, possible: 50, correct: 40)
+    @sitting2 = Sitting.create!(student_id: 1, quiz_id: 1, possible: 50, correct: 40)
+
+    teacher2 = Teacher.create!(first_name: "Alexis", last_name: "Lassus", password: "12345678", email: "alassus@gmail.com")
+
+section3 = Section.create!(name: "6th Grade Math 1st Hour", teacher: teacher2, subject: "Math", grade: "6")
+section4 = Section.create!(name: "6th Grade Math 2nd Hour", teacher: teacher2, subject: "Math", grade: "6")
+section5 = Section.create!(name: "6th Grade Math 5th Hour", teacher: teacher2, subject: "Math", grade: "6")
+
+[section3, section4, section5].each do |section|
+  students = Student.all
+  students.each { |student| section.students << student }
+
+  section.requirements.each do |requirement|
+    next if rand(0..50) == 0
+
+    quiz = Quiz.create!(name: Faker::Lorem.words(3).join(" "), instructions: "Do these, children!", section: section, requirement: requirement)
+    students.each do |student|
+      n = [1, 2].sample
+      n.times { Sitting.create!(student: student, quiz: quiz, possible: 10, correct: rand(4..11)) }
+    end
+
+  end
+
+end
+
+
+
+
+
+
+
+
+
   end
 
 
@@ -31,15 +69,21 @@ RSpec.describe Teacher, :type => :model do
   		expect(sections[0].grade).to eq("K") 
   	end
 
-  	it "should be able to get standards by teacher" do 
-			sections_and_standards = Teacher.get_available_sections_and_standards(1)
+  	xit "should be able to get standards by teacher" do 
+      sections_and_standards = Teacher.get_available_sections_and_standards(1)
 			standards = sections_and_standards[:standards]
-			expect(standards[0][0]).to eq("K.RL.1") 
+			expect(sections_and_standards).to eq("K.RL.1") 
   	end
   end
 
   describe "get mastery_for_all_sections method" do 
   	it "should be able to get section mastery" do 
+      @masteries = Teacher.find_by(id:1).mastery_for_all_sections
+      @masteries = @liberty.mastery_for_all_sections
+      # expect(masteries[0][:section]).to eq("Yabadabadoo")
+      expect(@liberty).to eq("Yabadabadoo")
+    end
+  end
   		
 
 
