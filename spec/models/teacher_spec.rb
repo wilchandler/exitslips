189742728042e6f3ac3b341129d5ef5c904a6331
Lovinder Pnag <lspnag@gmail.com>
2014-10-23 15:29:32 -0500

    fixed teacher model tests

--- spec/models/teacher_spec.rb
+++ spec/models/teacher_spec.rb
@@ -11,69 +11,88 @@ RSpec.describe Teacher, :type => :model do
 
 
 	before(:each) do
-		@teacher = Teacher.create!(first_name: "Liberty", last_name: "Valance", email: "yahoo@gmail.com", password: "12345678")
-    @section = Section.create!(teacher_id: @teacher.id, name: "Yabadabadoo", grade: "K", subject: "Math")
-    @standard = Standard.create!(subject: "Math", grade: "K", description: "really hard stuff", code: "K.RL.1")
-    @requirement = Requirement.create!(standard_id: @standard.id, section_id: @section_id)
-    @student1 = Student.create!(first_name: "student", last_name: "1", email: "tester1@yahoo.com", password: "12345678")
-    @student2 = Student.create!(first_name: "student1", last_name: "2", email: "tester2@yahoo.com", password: "12345678")
-    @student3 = Student.create!(first_name: "student2", last_name: "3", email: "tester3@yahoo.com", password: "12345678")
-    @enrollment = Enrollment.create!(student_id: @student1.id, section_id: @section.id)
-    @quiz = Quiz.create!(name: "MyQuiz", instructions: "My Instructions", section_id: @section.id, requirement_id: @requirement.id)
-    @quiz2 = Quiz.create!(name: "MyQuiz2", instructions: "My Instructions", section_id: @section.id, requirement_id: @requirement.id)
+  
 
-    @sitting = Sitting.create!(student_id: @student1.id, quiz_id: @quiz.id, possible: 50, correct: 40, graded: true)
-    @sitting2 = Sitting.create!(student_id: @student1.id, quiz_id: @quiz.id, possible: 50, correct: 40, graded: true)
+      standard = Standard.create!(subject: "ELA", grade: "5th", code: "boo", description: "hard stuff")
+      standard = Standard.create!(subject: "Math", grade: "5th", code: "boo", description: "really hard stuff")
 
-    teacher2 = Teacher.create!(first_name: "Alexis", last_name: "Lassus", password: "12345678", email: "alassus@gmail.com")
 
-    section3 = Section.create!(name: "6th Grade Math 1st Hour", teacher: teacher2, subject: "Math", grade: "6")
-    section4 = Section.create!(name: "6th Grade Math 2nd Hour", teacher: teacher2, subject: "Math", grade: "6")
-    section5 = Section.create!(name: "6th Grade Math 5th Hour", teacher: teacher2, subject: "Math", grade: "6")
 
-        [section3, section4, section5].each do |section|
-          students = Student.all
-          students.each { |student| section.students << student }
 
-          section.requirements.each do |requirement|
-            next if rand(0..50) == 0
+      teacher = Teacher.create!(first_name: "Casey", last_name: "Cumbow", password: "boom1234", email: "case-eee@yahoo.com")
+      math = Section.create!(name: "5th Period Math", teacher: teacher, subject: "Math", grade: "4" )
+      english = Section.create!(name: "3rd Period English", teacher: teacher, subject: "ELA", grade: "4" )
 
-            quiz = Quiz.create!(name: Faker::Lorem.words(3).join(" "), instructions: "Do these, children!", section: section, requirement: requirement)
-            students.each do |student|
-              n = [1, 2].sample
-              n.times { Sitting.create!(student: student, quiz: quiz, possible: 10, correct: rand(4..11)) }
-            end
+      math_smarts = [
+        Student.new(first_name: "Grace", last_name: "Hopper", email: "student1@yahoo.com", password: "12345678"),
+        Student.new(first_name: "Marie", last_name: "Curie", email: "student2@yahoo.com", password: "12345678")
+      ]
 
-          end
+      english_smarts = [
+        Student.new(first_name: "Virginia", last_name: "Woolf", email: "student9@yahoo.com", password: "12345678"),
+        Student.new(first_name: "David", last_name: "Foster Wallace", email: "student10@yahoo.com", password: "12345678")
+      ]
 
+      students = (math_smarts + english_smarts).shuffle
+
+      students.sort_by{ |s| s.last_name }.each{ |s| s.save }
+
+      students.each do |student|
+        math.students << student
+        english.students << student
+      end
+
+      math.requirements.each_with_index do |r, i|
+        standard = r.standard
+        math.quizzes << q = Quiz.create!(
+          requirement: r,
+          name: "Quiz on #{standard.code}",
+          instructions: standard.description
+        )
+
+        students.each do |s|
+          num_correct = (math_smarts.include? s) ? rand(6..10) : rand(4..8)
+          num_correct -= 3 if standard.code == "4.NF.3.a" && !(s.first_name == "Ada" || s.first_name == "Grace") # WRITE QUESTIONS!
+          num_correct -= 2 if standard.code == "4.NBT.4"
+          num_correct -= 1 if s.first_name == "Ernest"
+          num_correct += 1 if s.first_name == "Virginia"
+
+          Sitting.create!(
+            student: s,
+            quiz: q,
+            possible: 10,
+            correct: num_correct,
+            graded: true
+          )
         end
 
-      sections_and_standards = Teacher.get_available_sections_and_standards(1)
-      standards = sections_and_standards[:standards]
+      end
+
 
   end
 
 
   context "get_available_sections_and_standards method" do 
-  	xit "should be able to get sections by teacher" do 
-  		sections_and_standards = Teacher.get_available_sections_and_standards(1)
-  		sections = sections_and_standards[:sections]
-  		expect(sections[0].name).to eq("Yabadabadoo") 
-  		expect(sections[0].subject).to eq("Math") 
-  		expect(sections[0].grade).to eq("K") 
+  	it "should be able to get sections by teacher" do 
+  		teacher = Teacher.first
+      sections_and_standards = Teacher.get_available_sections_and_standards(teacher.id)
+      sections = sections_and_standards[:sections]
+      expect(sections[0].name).to eq("3rd Period English") 
+      expect(sections[0].subject).to eq("ELA") 
+      expect(sections[0].grade).to eq("4") 
   	end
 
   	xit "should be able to get standards by teacher" do 
-      sections_and_standards = Teacher.get_available_sections_and_standards(1)
+      teacher = Teacher.first
+      sections_and_standards = Teacher.get_available_sections_and_standards(teacher.id)
 			standards = sections_and_standards[:standards]
 			expect(sections_and_standards).to eq("K.RL.1") 
   	end
 
-  	xit "should be able to get section mastery" do 
-      @liberty = Teacher.find_by(first_name: "Liberty").mastery_for_all_sections
-      @masteries = @liberty.mastery_for_all_sections
-      # expect(masteries[0][:section]).to eq("Yabadabadoo")
-      expect(@masteries).to eq("Yabadabadoo")
+  	it "should be able to get section mastery" do 
+      teacher = Teacher.first
+      @masteries = teacher.mastery_for_all_sections
+      expect(@masteries[0][:section]).to eq("3rd Period English")
     end
   end
   		
