b60c718a78e3e8176f529e59d77963951ead3ef1
Wil Chandler <wilchandler2@gmail.com>
2014-10-22 19:24:56 -0500

    Added (possibly expensive and weak) validation of quiz submission

--- app/controllers/sittings_controller.rb
+++ app/controllers/sittings_controller.rb
@@ -9,6 +9,8 @@ class SittingsController < ApplicationController
     responses = params[:response]
 
     quiz = Quiz.includes(:section).find_by(id: params[:quiz_id])
+    student = Student.find_by(id: current_user_id)
+    sittings_beginning_count = student.sittings.count
 
     Sitting.grade_response({
       responses: responses,
@@ -16,7 +18,12 @@ class SittingsController < ApplicationController
       student_id: current_user_id
     })
 
-    flash[:notice] = "Quiz successfully submitted!"
+    sittings_new_count = student.sittings.count
+    if sittings_new_count > sittings_beginning_count
+      flash[:notice] = "Quiz successfully submitted!"
+    else
+      flash[:error] = "Something went wrong..."
+    end
     redirect_to section_path(quiz.section)
   end
 end
